a4b788e3f8cdbd7532a0ab8faf8fe69099d9d503
Harrison <hmeinertrita@gmail.com>
2018-10-05 19:05:32 -0400

    remove body-parser dependancy

--- express-server.js
+++ express-server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const bot = require('./bot.js');
 const admin = require('./admin.js');
 const conversation = require('./conversation.js');
