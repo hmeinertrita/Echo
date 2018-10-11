8f35445773242f2c18a052f1ab769fcb6ab831d9
Harrison Meinert-Rita <hmeinertrita@gmail.com>
2018-10-11 12:05:47 -0400

    updated discord profile management

--- echo/modules/discord/commands.js
+++ echo/modules/discord/commands.js
@@ -2,17 +2,18 @@ const commands = require('../../commands.js');
 const discordConversations = require('../../commands.js');
 
 module.exports = (client, server) => {
-  const discordProfile = new commands.Command('discord-profile', function(profileName) {
-    const path = "./profiles/" + profileName + '/';
-    const profile = JSON.parse(fs.readFileSync(path + 'profile.json'));
-    client.user.setAvatar(path + profile.avatar);
-    if(server) {server.me.setNickname(profile.nickname);}
-    this.log("Profile " + profileName + " loaded!");
+  const discordProfile = new commands.Command('discord-profile', function() {
+    client.user.setAvatar(this.profilePath + this.profile.id + '/' + this.profile.avatar);
+    if(server) {
+      server.me.setNickname(this.profile.nickname);
+      this.log('Set discord server nickname to: '+this.profile.nickname);
+    }
+    this.log("Discord bot now using profile: " + this.profile.id);
   });
 
   const addDiscordChannel = new commands.Command('add-channel', function(id) {
     this.addConversation(new discordConversations.DiscordConversation(client.channels.get(id), client.user.id));
-    this.log("Added channel: " + client.channels.get(id).id);
+    this.log("Added discord channel: " + client.channels.get(id).id);
   });
 
   return [discordProfile, addDiscordChannel];
